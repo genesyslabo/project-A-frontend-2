e912d034e33a6ad4c9fafd1b4697e93566961c5e
Larry <chenhbc@gmail.com>
2023-06-07 00:07:58 +0800

    add production contract address

--- src/common/constants.tsx
+++ src/common/constants.tsx
@@ -1,10 +1,10 @@
-export const MetaflareContractAddr = "0x4d2A4736f0213CD78a910c395Da007675a83F0fB";
+export const MetaflareContractAddr = process.env.NEXT_PUBLIC_CONTRACT_MF; //"0x4d2A4736f0213CD78a910c395Da007675a83F0fB";
 
-export const NftContractAddr = "0x6bFD38a2b36a8E310361e3Eb9d834d35e52B1896";
+export const NftContractAddr = process.env.NEXT_PUBLIC_CONTRACT_NFT;//"0x6bFD38a2b36a8E310361e3Eb9d834d35e52B1896";
 
-export const StakingFlexibleContractAddr = "0x9c31B722963b0551846EFFfE1129fCbB43661BC6";
+export const StakingFlexibleContractAddr = process.env.NEXT_PUBLIC_CONTRACT_FLEXIBLE;//"0x9c31B722963b0551846EFFfE1129fCbB43661BC6";
 
-export const StakingLockContractAddr = "0xe1243725ddD71478456E6D6E8B197D3E7B155290";
+export const StakingLockContractAddr = process.env.NEXT_PUBLIC_CONTRACT_LOCK; //"0xe1243725ddD71478456E6D6E8B197D3E7B155290";
 
 export const MinStakingAmount = 1000;
 
